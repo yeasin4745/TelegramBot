a44af0b555acc6df40612d1b7fc82fe1eb0662e6
MD Yeasin Ali <yeasinali4745@gmail.com>
2026-01-16 23:20:35 +0600

    Create helpers.js

---
+++ utils/helpers.js
@@ -0,0 +1,19 @@
+function formatDate(date) {
+    return new Date(date).toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+}
+
+function truncateText(text, maxLength = 100) {
+    if (text.length <= maxLength) return text;
+    return text.substring(0, maxLength) + '...';
+}
+
+module.exports = {
+    formatDate,
+    truncateText
+};
